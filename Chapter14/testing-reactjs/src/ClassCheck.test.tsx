ce4d2edc656343eedc85ac48284db96d624ed506
Mikhail Sakhniuk <sakhnyukmikhail@gmail.com>
2025-06-23 15:43:48 +0200

    Chapter 14

---
+++ Chapter14/testing-reactjs/src/ClassCheck.test.tsx
@@ -0,0 +1,15 @@
+import "@testing-library/jest-dom/vitest";
+import { render, screen, fireEvent } from "@testing-library/react";
+import { describe, it, expect } from "vitest";
+import { ClassCheck } from "./ClassCheck";
+
+describe("ClassCheck", () => {
+  it("should have class active when button was clicked", () => {
+    render(<ClassCheck />);
+    const button = screen.getByRole("button");
+
+    expect(button).not.toHaveClass("active");
+    fireEvent.click(button);
+    expect(button).toHaveClass("active");
+  });
+});
