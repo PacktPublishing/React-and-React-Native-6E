a2fbc1f507749d755784db5c5a6a8ca96c8bdc9e
Mikhail Sakhniuk <sakhnyukmikhail@gmail.com>
2025-06-23 15:31:18 +0200

    Chapter 13

---
+++ Chapter13/using-nextjs/next.config.ts
@@ -0,0 +1,11 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [new URL("https://avatars.githubusercontent.com/u/**")],
+  },
+};
+
+export default nextConfig;
