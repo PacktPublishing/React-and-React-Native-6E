a784d55def7a92f51f18d59feedb604354ede555
Rodrigo Lobenwein <rodrigolobenweindev@gmail.com>
2025-10-25 15:12:31 -0300

    Enhance InputField component to provide a visible output for the reader and maintain consistency

--- Chapter06/2-using-typescript-in-react/src/InputField.tsx
+++ Chapter06/2-using-typescript-in-react/src/InputField.tsx
@@ -1,13 +1,18 @@
 import React from "react";
 
 const InputField = () => {
-  const [value, setValue] = React.useState("");
+  const [value, setValue] = React.useState<string>("");
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setValue(event.target.value);
   };
 
-  return <input value={value} onChange={handleChange} />;
+  return (
+    <>
+      <input value={value} onChange={handleChange} />
+      <p>Value: {value}</p>
+    </>
+  );
 };
 
 export default InputField;
