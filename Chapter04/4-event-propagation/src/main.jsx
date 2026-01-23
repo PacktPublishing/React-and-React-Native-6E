99619c2b1195a7fcbe76c2ea0635fb8704c748c3
Rodrigo Lobenwein <rodrigolobenweindev@gmail.com>
2026-01-23 19:54:49 -0300

    Chapter 04: add event propagation example project

---
+++ Chapter04/4-event-propagation/src/main.jsx
@@ -0,0 +1,6 @@
+import { createRoot } from 'react-dom/client'
+import EventPropagationDemo from './EventPropagation.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <EventPropagationDemo />
+)
