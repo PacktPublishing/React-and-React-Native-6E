36f29232210f7e546ea3892527c1edd410ccf0ef
Rodrigo Lobenwein <rodrigolobenweindev@gmail.com>
2026-01-24 11:41:22 -0300

    remove unnecessary commas

--- Chapter02/01-hello-jsx/src/main.jsx
+++ Chapter02/01-hello-jsx/src/main.jsx
@@ -3,5 +3,5 @@ import { createRoot } from "react-dom/client";
 createRoot(document.getElementById("root")).render(
   <p>
     Hello, <strong>JSX</strong>
-  </p>,
+  </p>
 );
--- Chapter03/12-usedeferredvalue-hook/src/main.jsx
+++ Chapter03/12-usedeferredvalue-hook/src/main.jsx
@@ -2,5 +2,5 @@ import { createRoot } from "react-dom/client";
 import SearchInputWithDeferredResults from "./SearchInputWithDeferredResults";
 
 createRoot(document.getElementById("root")).render(
-  <SearchInputWithDeferredResults />,
+  <SearchInputWithDeferredResults />
 );
